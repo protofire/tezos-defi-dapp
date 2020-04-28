48f56857a9ac47cb32b80040087346d27c049b8e
Mariano Aguero <mariano.aguero@altoros.com>
2020-04-28 15:40:20 -0300

    Add contract hook

---
+++ defi-dapp/src/hooks/contracts.hook.ts
@@ -0,0 +1,42 @@
+import { useMemo } from 'react'
+import { InMemorySigner } from '@taquito/signer'
+
+import { useAsyncMemo } from 'use-async-memo'
+import { PoolService } from '../services/poolContract.service'
+import { TokenService } from '../services/tokenContract.service'
+import { AccountContext } from '../state/account.context'
+import {
+  POOL_CONTRACT_ADDRESS as poolContractAddress,
+  TOKEN_CONTRACT_ADDRESS as tokenContractAddress,
+  TEZOS_RPC as rpc,
+} from '../config/constants'
+
+export const useContracts = (context: AccountContext) => {
+  const { account } = context
+
+  const signer = account
+    ? InMemorySigner.fromFundraiser(account.email, account.password, account.mnemonic.join(' '))
+    : undefined
+
+  const poolService = useAsyncMemo(
+    async () => await PoolService.create(poolContractAddress, rpc, signer),
+    [poolContractAddress, rpc, signer],
+    null,
+  )
+
+  const tokenService = useAsyncMemo(
+    async () => await TokenService.create(tokenContractAddress, rpc, signer),
+    [poolContractAddress, rpc, signer],
+    null,
+  )
+
+  return useMemo(
+    () => ({
+      poolService,
+      tokenService,
+    }),
+    [poolService, tokenService],
+  )
+}
+
+export type Contracts = ReturnType<typeof useContracts>
