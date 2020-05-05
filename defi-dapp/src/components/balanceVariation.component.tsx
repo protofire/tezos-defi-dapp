c7e45f9f1e88aa0558059b3bd9c47881382a806d
Mariano Aguero <mariano.aguero@altoros.com>
2020-05-05 09:40:47 -0300

    Apply prettier

--- defi-dapp/src/components/balanceVariation.component.tsx
+++ defi-dapp/src/components/balanceVariation.component.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import BigNumber from 'bignumber.js'
 
-import {tzFormatter} from "../utils/tool"
+import { tzFormatter } from '../utils/tool'
 
 interface BalanceVariationItem {
-    amountFrom: BigNumber
-    amountTo: Maybe<BigNumber>
+  amountFrom: BigNumber
+  amountTo: Maybe<BigNumber>
 }
 
 export const BalanceVariationItem = (props: BalanceVariationItem) => {
-    const { amountFrom, amountTo } = props
-    return (
-        <>
-            {!amountTo && <label>{tzFormatter(amountFrom, 'tz')}</label>}
-            {amountTo && !amountTo.isZero() && (
-                <>
-                    <label>{tzFormatter(amountFrom, 'tz')}</label>
-                    &nbsp;
-                    <img
-                        src="https://icongr.am/feather/arrow-right.svg?size=16&amp;color=14854f"
-                        alt="icon"
-                    />
-                    &nbsp;
-                    <label>{tzFormatter(amountTo, 'tz')}</label>
-                </>
-            )}
-        </>
-    )
+  const { amountFrom, amountTo } = props
+  return (
+    <>
+      {!amountTo && <label>{tzFormatter(amountFrom, 'tz')}</label>}
+      {amountTo && !amountTo.isZero() && (
+        <div style={{ marginLeft: '-34px' }}>
+          <label>{tzFormatter(amountFrom, 'tz')}</label>
+          &nbsp;
+          <img
+            src="https://icongr.am/feather/arrow-right.svg?size=16&amp;color=14854f"
+            alt="icon"
+          />
+          &nbsp;
+          <label>{tzFormatter(amountTo, 'tz')}</label>
+        </div>
+      )}
+    </>
+  )
 }
