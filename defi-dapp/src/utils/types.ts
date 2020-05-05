1e068ab631a62b5c0ade40d41b13fddd179c2665
Mariano Aguero <mariano.aguero@altoros.com>
2020-05-05 09:34:24 -0300

    Add new type

--- defi-dapp/src/utils/types.ts
+++ defi-dapp/src/utils/types.ts
@@ -1,4 +1,13 @@
-export enum ModalAction {
-    Supply = 'Supply',
-    Withdraw = 'Withdraw',
+export enum Action {
+  Supply = 'Supply',
+  Withdraw = 'Withdraw',
+}
+
+export interface Account {
+  amount: string
+  email: string
+  mnemonic: Array<string>
+  password: string
+  pkh: string
+  secret: string
 }
