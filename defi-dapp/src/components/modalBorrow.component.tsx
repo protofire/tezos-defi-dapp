40812bb136640872246b67a10849be727c9719c0
Mariano Aguero <mariano.aguero@altoros.com>
2020-05-14 11:02:39 -0300

    Add some structure to the modal borrow component

--- defi-dapp/src/components/modalBorrow.component.tsx
+++ defi-dapp/src/components/modalBorrow.component.tsx
@@ -1,78 +1,167 @@
 import React, { HTMLAttributes, useState } from 'react'
+import { BigNumberInput } from 'big-number-input'
+import BigNumber from 'bignumber.js'
+import { useToasts } from 'react-toast-notifications'
+import Loader from 'react-loader-spinner'
 
 import { ModalWrapper } from './modalWrapper.component'
+import { GasEstimation } from './gasEstimation.component'
+import { Tab } from './tab.component'
+import { BalanceVariationItem } from './balanceVariation.component'
+import { PoolService } from '../services/poolContract.service'
+import { Action, Account } from '../utils/types'
+import { BorrowMessage, RepayBorrowMessage } from './messages.component'
+import { tzFormatter } from '../utils/tool'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   isOpen: boolean
   onClose: () => void
-}
-
-enum ModalAction {
-  Borrow = 'Borrow',
-  Repay = 'Repay',
+  poolService: PoolService
+  account: Account
 }
 
 export const ModalBorrow = (props: Props) => {
-  const { onClose, isOpen } = props
+  const { onClose, isOpen, poolService, account } = props
 
-  const [modalAction, setModalAction] = useState<ModalAction>(ModalAction.Borrow)
+  const { addToast } = useToasts()
+
+  const [amount, setAmount] = useState<Maybe<BigNumber>>(null)
+  const [transferAction, setTransferAction] = useState<Action>(Action.Borrow)
+  const [loadingTransferTransaction, setLoadingTransferTransaction] = useState<boolean>(false)
+
+  const setMax = async () => {
+    if (transferAction === Action.Borrow) {
+      // TODO add max amount
+    } else {
+      // TODO add max amount
+    }
+  }
+
+  const submit = async () => {
+    if (!amount) return
+
+    setLoadingTransferTransaction(true)
+    if (transferAction === Action.Borrow) {
+      try {
+        const content = <BorrowMessage hash={'COMPLETE'} amount={amount} />
+
+        addToast(content, { appearance: 'success', autoDismiss: true })
+
+        onClose()
+      } catch (err) {
+        // eslint-disable-next-line
+                console.error(err.message)
+        addToast(`There is an error borrowing an amount.`, {
+          appearance: 'error',
+          autoDismiss: true,
+        })
+      }
+    } else {
+      try {
+        const content = <RepayBorrowMessage hash={'COMPLETE'} amount={amount} />
+
+        addToast(content, { appearance: 'success', autoDismiss: true })
+
+        onClose()
+      } catch (err) {
+        // eslint-disable-next-line
+                console.error(err.message)
+        addToast(`There is an error paying a borrow.`, { appearance: 'error', autoDismiss: true })
+      }
+    }
+    setLoadingTransferTransaction(false)
+  }
+
+  const callbackBorrow = () => {
+    setTransferAction(Action.Borrow)
+    if (amount) {
+      setAmount(null)
+    }
+  }
+
+  const callbackRepayBorrow = () => {
+    setTransferAction(Action.RepayBorrow)
+    if (amount) {
+      setAmount(null)
+    }
+  }
+
+  const disableButtonSubmit = !amount || (amount && amount.isZero()) || loadingTransferTransaction
+
+  const disableButtonCancel = loadingTransferTransaction
 
   return (
     <ModalWrapper isOpen={isOpen} onRequestClose={onClose}>
       <div className="card">
         <header className="is-center">
-          <h4>{modalAction}</h4>
+          <h4>{transferAction}</h4>
         </header>
         <div className="row is-center">
           <nav className="tabs">
-            {/*eslint-disable-next-line*/}
-            <a
-              href=""
-              style={{ cursor: 'pointer' }}
-              onClick={() => setModalAction(ModalAction.Borrow)}
-              className={`${modalAction === ModalAction.Borrow ? 'active' : ''}`}
-            >
-              {ModalAction.Borrow}
-            </a>
-            {/*eslint-disable-next-line*/}
-            <a
-              style={{ cursor: 'pointer' }}
-              onClick={() => setModalAction(ModalAction.Repay)}
-              className={`${modalAction === ModalAction.Repay ? 'active' : ''}`}
-            >
-              {ModalAction.Repay}
-            </a>
+            <Tab
+              action={Action.Borrow}
+              callback={callbackBorrow}
+              active={transferAction === Action.Borrow}
+            />
+            <Tab
+              action={Action.RepayBorrow}
+              callback={callbackRepayBorrow}
+              active={transferAction === Action.RepayBorrow}
+            />
           </nav>
         </div>
         <div className="row" style={{ marginTop: '30px' }}>
           <div className="is-center">
-            <input />
-            <div className="button primary" style={{ marginLeft: '10px' }}>
+            <BigNumberInput
+              autofocus={true}
+              decimals={6}
+              onChange={newValue =>
+                newValue ? setAmount(new BigNumber(newValue)) : setAmount(null)
+              }
+              value={amount ? amount.toString() : ''}
+            />
+            <button
+              className="button primary"
+              disabled={!account}
+              onClick={setMax}
+              style={{ marginLeft: '10px' }}
+            >
               Max
-            </div>
+            </button>
           </div>
+        </div>
+        <div className={`row is-left}`}>
+          <span className="text-grey">Max amount allowed: </span>
         </div>
         <div className="row" style={{ marginTop: '30px' }}>
           <div className="col">
             <label>Borrow balance</label>
           </div>
           <div className="col is-right">
-            <label>0 tez</label>
+            <BalanceVariationItem amountFrom={new BigNumber(0)} amountTo={new BigNumber(0)} />
           </div>
         </div>
         <div className="row" style={{ marginTop: '5px' }}>
           <div className="col">
-            <label>Borrow limi</label>
+            <label>Borrow limit</label>
           </div>
           <div className="col is-right">
-            <label>0 tez</label>
+            <BalanceVariationItem amountFrom={new BigNumber(0)} amountTo={new BigNumber(0)} />
           </div>
         </div>
         <footer className="row is-right" style={{ marginTop: '30px' }}>
-          <div className="button primary">{modalAction}</div>
-          <div onClick={onClose} className="button">
+          <GasEstimation amount={amount} action={transferAction} poolService={poolService} />
+          <button
+            className="button primary"
+            disabled={disableButtonSubmit}
+            onClick={submit}
+            style={{ marginLeft: '1rem' }}
+          >
+            {transferAction}
+          </button>
+          <button disabled={disableButtonCancel} onClick={onClose} className="button" >
             Cancel
-          </div>
+          </button>
         </footer>
       </div>
     </ModalWrapper>
